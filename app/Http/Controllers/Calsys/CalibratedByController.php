cd7b9146564fa960322fd5b3cb3de64991cf8550
Rob Winkky <winkky@gmail.com>
2021-11-01 14:16:15 -0400

    calsys calibratedbys controller refactored

--- app/Http/Controllers/Calsys/CalibratedByController.php
+++ app/Http/Controllers/Calsys/CalibratedByController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Calsys;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\MassDestroyCalibratedByRequest;
-use App\Http\Requests\StoreCalibratedByRequest;
-use App\Http\Requests\UpdateCalibratedByRequest;
-use App\Models\CalibratedBy;
+use App\Http\Requests\Calsys\MassDestroyCalibratedByRequest;
+use App\Http\Requests\Calsys\StoreCalibratedByRequest;
+use App\Http\Requests\Calsys\UpdateCalibratedByRequest;
+use App\Models\Calsys\CalibratedBy;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,51 +15,51 @@ class CalibratedByController extends Controller
 {
     public function index()
     {
-        abort_if(Gate::denies('calibrated_by_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('calsys_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         $calibratedBies = CalibratedBy::all();
 
-        return view('admin.calibratedBies.index', compact('calibratedBies'));
+        return view('calsys.calibratedBies.index', compact('calibratedBies'));
     }
 
     public function create()
     {
-        abort_if(Gate::denies('calibrated_by_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('calsys_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.calibratedBies.create');
+        return view('calsys.calibratedBies.create');
     }
 
     public function store(StoreCalibratedByRequest $request)
     {
         $calibratedBy = CalibratedBy::create($request->all());
 
-        return redirect()->route('admin.calibrated-bies.index');
+        return redirect()->route('calsys.calibrated-bies.index');
     }
 
     public function edit(CalibratedBy $calibratedBy)
     {
-        abort_if(Gate::denies('calibrated_by_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('calsys_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.calibratedBies.edit', compact('calibratedBy'));
+        return view('calsys.calibratedBies.edit', compact('calibratedBy'));
     }
 
     public function update(UpdateCalibratedByRequest $request, CalibratedBy $calibratedBy)
     {
         $calibratedBy->update($request->all());
 
-        return redirect()->route('admin.calibrated-bies.index');
+        return redirect()->route('calsys.calibrated-bies.index');
     }
 
     public function show(CalibratedBy $calibratedBy)
     {
-        abort_if(Gate::denies('calibrated_by_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('calsys_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.calibratedBies.show', compact('calibratedBy'));
+        return view('calsys.calibratedBies.show', compact('calibratedBy'));
     }
 
     public function destroy(CalibratedBy $calibratedBy)
     {
-        abort_if(Gate::denies('calibrated_by_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('calsys_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         $calibratedBy->delete();
 
