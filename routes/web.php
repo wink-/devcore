f943e1ee18a8b7f243d92c21dde77be45bf3dc26
Rob Winkky <winkky@gmail.com>
2021-11-03 13:52:33 -0400

    moved routes into domain route groups

--- routes/web.php
+++ routes/web.php
@@ -12,27 +12,183 @@ Route::get('/home', function () {
 });
 
 Auth::routes(['register' => false]);
+
+//Calsys
+Route::group(['prefix' => 'calsys', 'as' => 'calsys.', 'namespace' => 'Calsys', 'middleware' => ['auth']], function () {
+
+    // Calibrated By
+    Route::delete('calibrated-bies/destroy', 'CalibratedByController@massDestroy')->name('calibrated-bies.massDestroy');
+    Route::resource('calibrated-bies', 'CalibratedByController');
+
+    // Employee
+    Route::delete('employees/destroy', 'EmployeeController@massDestroy')->name('employees.massDestroy');
+    Route::resource('employees', 'EmployeeController');
+
+    // Type
+    Route::delete('types/destroy', 'TypeController@massDestroy')->name('types.massDestroy');
+    Route::resource('types', 'TypeController');
+
+    // Location
+    Route::delete('locations/destroy', 'LocationController@massDestroy')->name('locations.massDestroy');
+    Route::resource('locations', 'LocationController');
+
+    // Owner
+    Route::delete('owners/destroy', 'OwnerController@massDestroy')->name('owners.massDestroy');
+    Route::resource('owners', 'OwnerController');
+
+    // Period
+    Route::delete('periods/destroy', 'PeriodController@massDestroy')->name('periods.massDestroy');
+    Route::resource('periods', 'PeriodController');
+
+    // Source
+    Route::delete('sources/destroy', 'SourceController@massDestroy')->name('sources.massDestroy');
+    Route::resource('sources', 'SourceController');
+
+    // Status
+    Route::delete('statuses/destroy', 'StatusController@massDestroy')->name('statuses.massDestroy');
+    Route::resource('statuses', 'StatusController');
+
+    // Device
+    Route::delete('devices/destroy', 'DeviceController@massDestroy')->name('devices.massDestroy');
+    Route::resource('devices', 'DeviceController');
+    
+});
+
+//Pacsys
 Route::group(['prefix' => 'pacsys', 'as' => 'pacsys.', 'namespace' => 'Pacsys', 'middleware' => ['auth']], function () {
+
     // Actions
     Route::delete('actions/destroy', 'ActionsController@massDestroy')->name('actions.massDestroy');
-    Route::resource('actions', 'ActionsController');    
-});    
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
-    Route::get('/', 'HomeController@index')->name('home');
-    // Permissions
-    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
-    Route::resource('permissions', 'PermissionsController');
+    Route::resource('actions', 'ActionsController');
 
-    // Roles
-    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
-    Route::resource('roles', 'RolesController');
+    // Actions Temp
+    Route::delete('actions-temps/destroy', 'ActionsTempController@massDestroy')->name('actions-temps.massDestroy');
+    Route::resource('actions-temps', 'ActionsTempController');
 
-    // Users
-    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
-    Route::resource('users', 'UsersController');
+    // Analysis
+    Route::delete('analysis/destroy', 'AnalysisController@massDestroy')->name('analysis.massDestroy');
+    Route::resource('analysis', 'AnalysisController');
 
-    // Audit Logs
-    Route::resource('audit-logs', 'AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+    // Analysis New
+    Route::delete('analysis-news/destroy', 'AnalysisNewController@massDestroy')->name('analysis-news.massDestroy');
+    Route::resource('analysis-news', 'AnalysisNewController');
+
+    // Calc Value
+    Route::delete('calc-values/destroy', 'CalcValueController@massDestroy')->name('calc-values.massDestroy');
+    Route::resource('calc-values', 'CalcValueController');
+
+    // Employee
+    Route::delete('employees/destroy', 'EmployeeController@massDestroy')->name('employees.massDestroy');
+    Route::resource('employees', 'EmployeeController');
+
+    // Form
+    Route::delete('forms/destroy', 'FormController@massDestroy')->name('forms.massDestroy');
+    Route::resource('forms', 'FormController');
+
+    // Form Type
+    Route::delete('form-types/destroy', 'FormTypeController@massDestroy')->name('form-types.massDestroy');
+    Route::resource('form-types', 'FormTypeController');
+
+    // Form Value Action
+    Route::delete('form-value-actions/destroy', 'FormValueActionController@massDestroy')->name('form-value-actions.massDestroy');
+    Route::resource('form-value-actions', 'FormValueActionController');
+
+    // Form Value Name
+    Route::delete('form-value-names/destroy', 'FormValueNameController@massDestroy')->name('form-value-names.massDestroy');
+    Route::resource('form-value-names', 'FormValueNameController');
+
+    // Form Value Period
+    Route::delete('form-value-periods/destroy', 'FormValuePeriodController@massDestroy')->name('form-value-periods.massDestroy');
+    Route::resource('form-value-periods', 'FormValuePeriodController');
+
+    // Form Value Range
+    Route::delete('form-value-ranges/destroy', 'FormValueRangeController@massDestroy')->name('form-value-ranges.massDestroy');
+    Route::resource('form-value-ranges', 'FormValueRangeController');
+
+    // Form Value Type
+    Route::delete('form-value-types/destroy', 'FormValueTypeController@massDestroy')->name('form-value-types.massDestroy');
+    Route::resource('form-value-types', 'FormValueTypeController');
+
+    // Form Value Unit
+    Route::delete('form-value-units/destroy', 'FormValueUnitController@massDestroy')->name('form-value-units.massDestroy');
+    Route::resource('form-value-units', 'FormValueUnitController');
+
+    // Form Value Visible
+    Route::delete('form-value-visibles/destroy', 'FormValueVisibleController@massDestroy')->name('form-value-visibles.massDestroy');
+    Route::resource('form-value-visibles', 'FormValueVisibleController');
+
+    // Months
+    Route::delete('months/destroy', 'MonthsController@massDestroy')->name('months.massDestroy');
+    Route::resource('months', 'MonthsController');
+
+    // Period
+    Route::delete('periods/destroy', 'PeriodController@massDestroy')->name('periods.massDestroy');
+    Route::resource('periods', 'PeriodController');
+
+    // Record
+    Route::delete('records/destroy', 'RecordController@massDestroy')->name('records.massDestroy');
+    Route::resource('records', 'RecordController');
+
+    // Scope
+    Route::delete('scopes/destroy', 'ScopeController@massDestroy')->name('scopes.massDestroy');
+    Route::resource('scopes', 'ScopeController');
+
+    // Security
+    Route::delete('securities/destroy', 'SecurityController@massDestroy')->name('securities.massDestroy');
+    Route::resource('securities', 'SecurityController');
+
+    // Unit
+    Route::delete('units/destroy', 'UnitController@massDestroy')->name('units.massDestroy');
+    Route::resource('units', 'UnitController');
+
+    // Validation
+    Route::delete('validations/destroy', 'ValidationController@massDestroy')->name('validations.massDestroy');
+    Route::resource('validations', 'ValidationController');    
+
+    // Years
+    Route::delete('years/destroy', 'YearsController@massDestroy')->name('years.massDestroy');
+    Route::resource('years', 'YearsController');    
+});
+
+//ProcessData
+Route::group(['prefix' => 'processdata', 'as' => 'processdata.', 'namespace' => 'ProcessData', 'middleware' => ['auth']], function () {
+
+    // Amp
+    Route::delete('amps/destroy', 'AmpController@massDestroy')->name('amps.massDestroy');
+    Route::resource('amps', 'AmpController');
+
+    // Record Types
+    Route::delete('record-types/destroy', 'RecordTypesController@massDestroy')->name('record-types.massDestroy');
+    Route::resource('record-types', 'RecordTypesController');
+
+    // Logger Types
+    Route::delete('logger-types/destroy', 'LoggerTypesController@massDestroy')->name('logger-types.massDestroy');
+    Route::resource('logger-types', 'LoggerTypesController');
+
+    // Pd Units
+    Route::delete('units/destroy', 'UnitsController@massDestroy')->name('units.massDestroy');
+    Route::resource('units', 'UnitsController');
+
+    // Pd Records
+    Route::delete('records/destroy', 'RecordsController@massDestroy')->name('records.massDestroy');
+    Route::resource('records', 'RecordsController');
+
+    // Loggers
+    Route::delete('loggers/destroy', 'LoggersController@massDestroy')->name('loggers.massDestroy');
+    Route::resource('loggers', 'LoggersController');
+
+    // Temperature
+    Route::delete('temperatures/destroy', 'TemperatureController@massDestroy')->name('temperatures.massDestroy');
+    Route::resource('temperatures', 'TemperatureController');
+
+    // Volt
+    Route::delete('volts/destroy', 'VoltController@massDestroy')->name('volts.massDestroy');
+    Route::resource('volts', 'VoltController');
+
+});
+
+//Wipsys
+Route::group(['prefix' => 'wipsys', 'as' => 'wipsys.', 'namespace' => 'Wipsys', 'middleware' => ['auth']], function () {
 
     // Carriers
     Route::delete('carriers/destroy', 'CarriersController@massDestroy')->name('carriers.massDestroy');
@@ -88,42 +244,6 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
 
     // Equipment
     Route::resource('equipment', 'EquipmentController', ['except' => ['destroy']]);
-
-    // Calibrated By
-    Route::delete('calibrated-bies/destroy', 'CalibratedByController@massDestroy')->name('calibrated-bies.massDestroy');
-    Route::resource('calibrated-bies', 'CalibratedByController');
-
-    // Calemployee
-    Route::delete('calemployees/destroy', 'CalemployeeController@massDestroy')->name('calemployees.massDestroy');
-    Route::resource('calemployees', 'CalemployeeController');
-
-    // Cal Type
-    Route::delete('cal-types/destroy', 'CalTypeController@massDestroy')->name('cal-types.massDestroy');
-    Route::resource('cal-types', 'CalTypeController');
-
-    // Cal Location
-    Route::delete('cal-locations/destroy', 'CalLocationController@massDestroy')->name('cal-locations.massDestroy');
-    Route::resource('cal-locations', 'CalLocationController');
-
-    // Cal Owner
-    Route::delete('cal-owners/destroy', 'CalOwnerController@massDestroy')->name('cal-owners.massDestroy');
-    Route::resource('cal-owners', 'CalOwnerController');
-
-    // Cal Period
-    Route::delete('cal-periods/destroy', 'CalPeriodController@massDestroy')->name('cal-periods.massDestroy');
-    Route::resource('cal-periods', 'CalPeriodController');
-
-    // Cal Source
-    Route::delete('cal-sources/destroy', 'CalSourceController@massDestroy')->name('cal-sources.massDestroy');
-    Route::resource('cal-sources', 'CalSourceController');
-
-    // Cal Status
-    Route::delete('cal-statuses/destroy', 'CalStatusController@massDestroy')->name('cal-statuses.massDestroy');
-    Route::resource('cal-statuses', 'CalStatusController');
-
-    // Cal Device
-    Route::delete('cal-devices/destroy', 'CalDeviceController@massDestroy')->name('cal-devices.massDestroy');
-    Route::resource('cal-devices', 'CalDeviceController');
 
     // Priorities
     Route::delete('priorities/destroy', 'PrioritiesController@massDestroy')->name('priorities.massDestroy');
@@ -189,127 +309,26 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::resource('workgroups', 'WorkgroupsController');
 
     // Ups
-    Route::resource('ups', 'UpsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+    Route::resource('ups', 'UpsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);    
+    
+});
 
-    // Actions Temp
-    Route::delete('actions-temps/destroy', 'ActionsTempController@massDestroy')->name('actions-temps.massDestroy');
-    Route::resource('actions-temps', 'ActionsTempController');
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    // Permissions
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
 
-    // Analysis
-    Route::delete('analysis/destroy', 'AnalysisController@massDestroy')->name('analysis.massDestroy');
-    Route::resource('analysis', 'AnalysisController');
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
 
-    // Analysis New
-    Route::delete('analysis-news/destroy', 'AnalysisNewController@massDestroy')->name('analysis-news.massDestroy');
-    Route::resource('analysis-news', 'AnalysisNewController');
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
 
-    // Calc Value
-    Route::delete('calc-values/destroy', 'CalcValueController@massDestroy')->name('calc-values.massDestroy');
-    Route::resource('calc-values', 'CalcValueController');
-
-    // P Employee
-    Route::delete('p-employees/destroy', 'PEmployeeController@massDestroy')->name('p-employees.massDestroy');
-    Route::resource('p-employees', 'PEmployeeController');
-
-    // Form
-    Route::delete('forms/destroy', 'FormController@massDestroy')->name('forms.massDestroy');
-    Route::resource('forms', 'FormController');
-
-    // Form Type
-    Route::delete('form-types/destroy', 'FormTypeController@massDestroy')->name('form-types.massDestroy');
-    Route::resource('form-types', 'FormTypeController');
-
-    // Form Value Action
-    Route::delete('form-value-actions/destroy', 'FormValueActionController@massDestroy')->name('form-value-actions.massDestroy');
-    Route::resource('form-value-actions', 'FormValueActionController');
-
-    // Form Value Name
-    Route::delete('form-value-names/destroy', 'FormValueNameController@massDestroy')->name('form-value-names.massDestroy');
-    Route::resource('form-value-names', 'FormValueNameController');
-
-    // Form Value Period
-    Route::delete('form-value-periods/destroy', 'FormValuePeriodController@massDestroy')->name('form-value-periods.massDestroy');
-    Route::resource('form-value-periods', 'FormValuePeriodController');
-
-    // Form Value Range
-    Route::delete('form-value-ranges/destroy', 'FormValueRangeController@massDestroy')->name('form-value-ranges.massDestroy');
-    Route::resource('form-value-ranges', 'FormValueRangeController');
-
-    // Form Value Type
-    Route::delete('form-value-types/destroy', 'FormValueTypeController@massDestroy')->name('form-value-types.massDestroy');
-    Route::resource('form-value-types', 'FormValueTypeController');
-
-    // Form Value Unit
-    Route::delete('form-value-units/destroy', 'FormValueUnitController@massDestroy')->name('form-value-units.massDestroy');
-    Route::resource('form-value-units', 'FormValueUnitController');
-
-    // Form Value Visible
-    Route::delete('form-value-visibles/destroy', 'FormValueVisibleController@massDestroy')->name('form-value-visibles.massDestroy');
-    Route::resource('form-value-visibles', 'FormValueVisibleController');
-
-    // Months
-    Route::delete('months/destroy', 'MonthsController@massDestroy')->name('months.massDestroy');
-    Route::resource('months', 'MonthsController');
-
-    // Period
-    Route::delete('periods/destroy', 'PeriodController@massDestroy')->name('periods.massDestroy');
-    Route::resource('periods', 'PeriodController');
-
-    // Record
-    Route::delete('records/destroy', 'RecordController@massDestroy')->name('records.massDestroy');
-    Route::resource('records', 'RecordController');
-
-    // Scope
-    Route::delete('scopes/destroy', 'ScopeController@massDestroy')->name('scopes.massDestroy');
-    Route::resource('scopes', 'ScopeController');
-
-    // Security
-    Route::delete('securities/destroy', 'SecurityController@massDestroy')->name('securities.massDestroy');
-    Route::resource('securities', 'SecurityController');
-
-    // P Unit
-    Route::delete('p-units/destroy', 'PUnitController@massDestroy')->name('p-units.massDestroy');
-    Route::resource('p-units', 'PUnitController');
-
-    // Years
-    Route::delete('years/destroy', 'YearsController@massDestroy')->name('years.massDestroy');
-    Route::resource('years', 'YearsController');
-
-    // Amp
-    Route::delete('amps/destroy', 'AmpController@massDestroy')->name('amps.massDestroy');
-    Route::resource('amps', 'AmpController');
-
-    // Record Types
-    Route::delete('record-types/destroy', 'RecordTypesController@massDestroy')->name('record-types.massDestroy');
-    Route::resource('record-types', 'RecordTypesController');
-
-    // Logger Types
-    Route::delete('logger-types/destroy', 'LoggerTypesController@massDestroy')->name('logger-types.massDestroy');
-    Route::resource('logger-types', 'LoggerTypesController');
-
-    // Pd Units
-    Route::delete('pd-units/destroy', 'PdUnitsController@massDestroy')->name('pd-units.massDestroy');
-    Route::resource('pd-units', 'PdUnitsController');
-
-    // Pd Records
-    Route::delete('pd-records/destroy', 'PdRecordsController@massDestroy')->name('pd-records.massDestroy');
-    Route::resource('pd-records', 'PdRecordsController');
-
-    // Loggers
-    Route::delete('loggers/destroy', 'LoggersController@massDestroy')->name('loggers.massDestroy');
-    Route::resource('loggers', 'LoggersController');
-
-    // Temperature
-    Route::delete('temperatures/destroy', 'TemperatureController@massDestroy')->name('temperatures.massDestroy');
-    Route::resource('temperatures', 'TemperatureController');
-
-    // Volt
-    Route::delete('volts/destroy', 'VoltController@massDestroy')->name('volts.massDestroy');
-    Route::resource('volts', 'VoltController');
-
-    // Validation
-    Route::delete('validations/destroy', 'ValidationController@massDestroy')->name('validations.massDestroy');
-    Route::resource('validations', 'ValidationController');
+    // Audit Logs
+    Route::resource('audit-logs', 'AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
 
     Route::get('global-search', 'GlobalSearchController@search')->name('globalSearch');
 });
